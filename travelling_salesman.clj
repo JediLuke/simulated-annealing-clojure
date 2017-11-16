6bc81e49f07858b7d38bc2f4117e0759ecf519c2
Luke Taylor <luke.taylor@heliansys.com>
2017-11-17 00:09:10 +1100

    Algorithm is working, but is clearly only halfway between being translated from the OO style pseudocode I had to a more functonal style. I have tested the operation using simple equations for cost/energy and state transformation such as linear equations.

--- travelling_salesman.clj
+++ travelling_salesman.clj
@@ -1,40 +1,73 @@
-(print "Initiate: simulated annealing.")
+(print "\nInitiate: simulated annealing.\n\n")
+
+;
+; Initial setup
 
 
-; Function to compute the "energy" or "cost" of a current state
-(defn calcEnergy [phi, state]
-   (phi state))
+(def initialState 72)
+(def initialTemp 10)
 
-; Given current state, go to the next one according to some transform function
-(defn generateNextState [state, transform]
-   (transform state))
+; returns a function which will calculate the y value for any x input value on the line y = mx + b
+(defn lineFunction [m b]
+   (fn [x] (+ (* m x) b)))
 
-; Exponent function
-(defn exp [x n]
-   (reduce * (repeat n x))) 
+; The following two parameters must be defined as *functions*
+(def phi (fn [x] (* x 2)))          ; "energy" or "cost function"
+(def stateFn (lineFunction 2 3))    ; function which takes in a state and returns the next state
+
+
+;
+; These functions are used in the higher level algorithm
+(defn calcEnergy [state, temp] (phi state))     ; Returns a function which calculates the energy of a state according to some energy function phi
+(defn nextState [state] (stateFn state))        ; Returns a function which transforms states according to the initialization function provided
 
 ; Compute the probability of moving from one state to the next, given the Temperature of the system
-(defn probMove [curState, nextState, tempConstant]
+(defn probMove [state, nextState, tempConstant]
+   ; Exponent function
+   (defn exp [x n]
+      (reduce * (repeat n x)))
    ; This function is a Boultzmann distribution
    (exp [2.718 (/
-      (- nextState curState)
+      (- nextState state)
       (* tempConstant))]))
 
 
-; Initial configuration
+;
+; Simulated annealing
 
-; Initial temperature
+; Start a SA search from a given start state, at a given start temp, cooling down until tempMin has been reached
+(defn simAnneal [state temp tempMin]
+   
+   ; Base case - return state when system "cools down" to the point where search stops, i.e. temp <= tempMin
+   (if (<= temp tempMin)
+      (do
+         (print "\n\n\nSystem cooldown achieved.\n")
+         state)
+      ; else -  Recursive case
+      (let [curEnergy (calcEnergy state temp)]                          ; Compute current energy level for current state
+         (print "Current state:" state "Current temp:" temp "\n")
+         ;(print "CurEnergy:" curEnergy "\n")
+         (let [nextState (nextState state)]                             ; Apply the state transform function to the current state
+            ;(print "nextState:" nextState "\n")
+            (let [nextStateEnergy (calcEnergy nextState temp)]          ; Compute energy for next state
+               ;(print "nextStateEnergy:" nextStateEnergy "\n")
+               (let [deltaE (- nextStateEnergy curEnergy)]              ; Calculate deltaE
+                  (if (compare deltaE 0)                                ; if (deltaE > 0) ; If next state is more optimal
+                     (simAnneal nextState (- temp 1) tempMin)                 ;     return nextState
+                     ((if (probMove state nextState temp) > 0)          ; elsif boulztMan(params) > rand(0,1)
+                        (simAnneal nextState (- temp 1) tempMin)              ;     Move to worse state with probability dependent on temperature
+                        (simAnneal state     (- temp 1) tempMin))                 ; otherwise (i.e. prob of new state not high enough), return currentState
+                  )
+               )
+            )
+         )
+      )
+   )
+)
+
 
 ; For tMax to tMin
-
-   ; Compute current energy level for current state
-   ; Compute next state
-   ; Compute energy for next state
-   ; Calculate deltaE
-   ; if (deltaE > 0) ; If next state is more optimal
-      ; return nextState
-   ; elsif boulztMan(params) > rand(0,1) ; Move to worse state with propability dependent on temperature
-      ; return currentState
+(print "Final state: " (simAnneal initialState initialTemp 0))
 
    
 ;-----------------------------------------------------------------------
