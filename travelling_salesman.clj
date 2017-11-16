214c18366469bc99d8776a6cce2cc5036d7216ce
Luke Taylor <luke.taylor@heliansys.com>
2017-11-16 20:55:34 +1100

    References added

--- travelling_salesman.clj
+++ travelling_salesman.clj
@@ -1,2 +1,21 @@
 (def x "Hello, World!")
 (print x)
+
+
+
+;-----------------------------------------------------------------------
+
+;  References
+;  ----------
+
+;     Simulated annealing
+
+;     - https://www.gnu.org/software/gsl/manual/html_node/Simulated-Annealing-algorithm.html
+;     - https://www.youtube.com/watch?v=eBmU1ONJ-os
+;     - http://kaygun.tumblr.com/post/59316257966/simulated-annealing-in-lisp
+;     - https://www.wikipedia.com/en/Simulated_annealing
+;     - https://www.wikipedia.com/en/Metropolis%E2%80%93Hastings_algorithm
+
+;     Clojure
+
+;     - https://learnxinyminutes.com/docs/clojure/
